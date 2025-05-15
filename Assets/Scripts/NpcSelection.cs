ab55c8e7d9b22bb0bf47cb5c3f5a91f22c214ed8
Dave Murray <138507286+Dave-Murray98@users.noreply.github.com>
2025-05-15 22:42:33 +0100

    Fixed NPC selector and added rough pop up

--- Assets/Scripts/NpcSelection.cs
+++ Assets/Scripts/NpcSelection.cs
@@ -1,23 +1,76 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPCSelectable : MonoBehaviour
 {
-    private NPCDataHolder dataHolder;
+    [Header("References")]
+    [SerializeField] private Canvas selectionCanvas;   // The world space canvas with buttons
+    [SerializeField] private Button arrestButton;      // The arrest button
+    [SerializeField] private Button cancelButton;      // The cancel button
 
-    private void Start()
+    private NPCDataHolder dataHolder;
+    private bool isSelected = false;
+
+    private void Awake()
     {
         dataHolder = GetComponent<NPCDataHolder>();
+
+        // Hide the selection canvas initially
+        if (selectionCanvas != null)
+        {
+            selectionCanvas.gameObject.SetActive(false);
+        }
+
     }
 
     private void OnMouseDown()
     {
-        if (GameManager.Instance != null && RoundManager.Instance != null)
+        Debug.Log("NPC clicked: " + gameObject.name);
+        if (GameManager.Instance == null) return;
+
+
+        // Toggle selection state
+        isSelected = !isSelected;
+
+        // Show/hide selection canvas
+        if (selectionCanvas != null)
         {
-            // Only allow selection during playing state
-            if (RoundManager.Instance.CurrentState == GameState.Playing)
+            selectionCanvas.gameObject.SetActive(isSelected);
+        }
+    }
+
+    public void OnArrestButtonClicked()
+    {
+        if (GameManager.Instance != null)
+        {
+            // Process the arrest
+            GameManager.Instance.CheckNPCSelection(gameObject);
+
+            // Hide selection canvas
+            isSelected = false;
+            if (selectionCanvas != null)
             {
-                GameManager.Instance.CheckNPCSelection(gameObject);
+                selectionCanvas.gameObject.SetActive(false);
             }
+        }
+    }
+
+    public void OnCancelButtonClicked()
+    {
+        // Just hide the selection canvas
+        isSelected = false;
+        if (selectionCanvas != null)
+        {
+            selectionCanvas.gameObject.SetActive(false);
+        }
+    }
+
+    // Optional: Hide selection UI when NPC is disabled/destroyed
+    private void OnDisable()
+    {
+        if (selectionCanvas != null)
+        {
+            selectionCanvas.gameObject.SetActive(false);
         }
     }
 }
